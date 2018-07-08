a1e282bd9162c072444e7f985b5cfc9c55fd23fa
narlah <mitrandir@gmail.com>
2018-07-08 23:38:53 +0300

    avoid adding zero url

--- src/NkUrlShortener/src/main/resources/static/js/shortUrl.js
+++ src/NkUrlShortener/src/main/resources/static/js/shortUrl.js
@@ -1,16 +1,20 @@
 var mainApp = angular.module("shortUrl", []);
 mainApp.controller('shortyC', function($scope, $http) {
-
+    var shortyC = $scope;
     $scope.shorten = function(){
-    var shortyC = this;
-        var res = $http.post('/smallUrl', {"requestThis":$scope.requestThis})
-         .then(function onSuccess(response) {
-            shortyC.shortenedUrl = response.data.shortenedUrl;
-            shortyC.originalUrl = response.data.originalUrl;
-          }).catch(function onError(response) {
-            shortyC.shortenedUrl = "Error " + response.status + response.statusText + response.headers;
-            shortyC.originalUrl = response.data.originalUrl;
-          });
+        if ($scope.requestThis != undefined && $scope.requestThis.length >= 3) {
+            var res = $http.post('/smallUrl', {"requestThis":$scope.requestThis})
+             .then(function onSuccess(response) {
+                shortyC.shortenedUrl = response.data.shortenedUrl;
+                shortyC.originalUrl = response.data.originalUrl;
+              }).catch(function onError(response) {
+                shortyC.shortenedUrl = "Error " + response.status + response.statusText + response.headers;
+                shortyC.originalUrl = response.data.originalUrl;
+              });
+        } else {
+            shortyC.originalUrl = "Must be over 2 characters!";
+            shortyC.shortenedUrl = "And can't be empty string!";
+        }
         $scope.requestThis = "";
     };
 });
