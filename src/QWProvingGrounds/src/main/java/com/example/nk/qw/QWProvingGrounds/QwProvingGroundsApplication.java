04617583ed327c512df64578f3c90c5d7bea3044
narlah <mitrandir@gmail.com>
2018-07-11 22:30:29 +0300

    initial commit for QWProvingGrounds

---
+++ src/QWProvingGrounds/src/main/java/com/example/nk/qw/QWProvingGrounds/QwProvingGroundsApplication.java
@@ -0,0 +1,12 @@
+package com.example.nk.qw.QWProvingGrounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwProvingGroundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwProvingGroundsApplication.class, args);
+	}
+}
